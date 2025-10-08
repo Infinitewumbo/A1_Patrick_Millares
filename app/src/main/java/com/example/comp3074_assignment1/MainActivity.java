42ed3e3394723b7cef75d8837669469b4e2ec475
Infinitewumbo <millareslepatrick@gmail.com>
2025-10-08 08:28:20 -0400

    Initialize views

--- app/src/main/java/com/example/comp3074_assignment1/MainActivity.java
+++ app/src/main/java/com/example/comp3074_assignment1/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.comp3074_assignment1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,29 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editHours, editRate;
+    private TextView textPay, textOvertimePay, textTotalPay, textTax;
+    private Button btnCalculate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        editHours = findViewById(R.id.edit_hours);
+        editRate = findViewById(R.id.edit_rate);
+        btnCalculate = findViewById(R.id.btn_calculate);
+        textPay = findViewById(R.id.text_pay);
+        textOvertimePay = findViewById(R.id.text_overtime_pay);
+        textTotalPay = findViewById(R.id.text_total_pay);
+        textTax = findViewById(R.id.text_tax);
+
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculatePay();
+            }
         });
     }
 }
