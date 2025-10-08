2b64a034bbcf6625815ee8632aee0f0651def29c
Infinitewumbo <millareslepatrick@gmail.com>
2025-10-08 08:22:43 -0400

    DetailsActivity

--- app/src/main/java/com/example/comp3074_assignment1/DetailsActivity.java
+++ app/src/main/java/com/example/comp3074_assignment1/DetailsActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {{
